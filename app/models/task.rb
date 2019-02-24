dc1c529e9655e09044b35ab35ab77e665ea0b1e4
Hm | edihasam <masa.ishi.right@gmail.com>
2019-02-24 13:09:51 +0900

    chapter 4-4

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,10 @@
 class Task < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 30 }
+  validate :validate_name_not_include_comma
+
+  private
+
+  def validate_name_not_include_comma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
 end
